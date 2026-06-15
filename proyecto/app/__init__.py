36d6bb5d52a7e040b87d3fc2eacb56eea635d2ed
Andre M <andr.munozd@duocuc.cl>
2026-06-15 14:54:30 -0400

    mm

--- proyecto/app/__init__.py
+++
@@ -1,8 +0,0 @@
-# Este archivo actúa como un agrupador de los controladores (rutas) subdivididos.
-# Permite mantener la estructura original sin romper los "url_for" de los templates.
-from . import main
-from . import auth
-from . import citas
-from . import admin
-from . import api
-from . import chatbot
